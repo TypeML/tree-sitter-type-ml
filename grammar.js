722aeeb230cdbcbe0d8ea57bff5e9fa5f70498d1
Irisu <mrapacheee@gmail.com>
2025-10-18 19:48:09 +0400

    definitions support

--- grammar.js
+++ grammar.js
@@ -1,7 +1,7 @@
-/**
+/*/**
  * @file TypeML grammar for tree-sitter
  * @author Irisu <mrapacheee@gmail.com>
- * @license MIT
+ * @license Apache-2.0
  */
 
 /// <reference types="tree-sitter-cli/dsl" />
@@ -12,51 +12,203 @@ module.exports = grammar({
 
   extras: ($) => [/[\s\t\r\n]/, $.comment_multi, $.comment_line],
 
-  conflicts: ($) => [],
-
   rules: {
     source_file: ($) =>
-      repeat(choice($.directive, $.struct_impl, $.expr_impl, $.element)),
+      repeat(
+        choice(
+          $.use_directive,
+          $.library_directive,
+          $.group_definition,
+          $.element_definition,
+          $.enum_definition,
+          $.expression_definition,
+          $.expression_impl,
+          $.struct_definition,
+          $.struct_impl,
+          $.element,
+        ),
+      ),
 
     comment_multi: ($) =>
       token(seq("/*", repeat(choice(/[^*]/, seq("*", /[^\/]/))), "*/")),
-
     comment_line: ($) => token(seq("//", repeat(/[^\n]/), /\n?/)),
 
-    integer: ($) => /-?[0-9]+/,
-    float: ($) => /-?[0-9]+\.[0-9]*/,
-    boolean: ($) => choice("true", "false"),
-    string: ($) => seq('"', /[^"]*/, '"'),
-    enum: ($) => /[a-zA-Z_][a-zA-Z0-9_]*/,
-    custom_enum: ($) => /[^"${}\[\],\/=> \s\t\r\n]+/,
-
-    directive: ($) =>
-      prec.right(
-        1,
-        seq(
-          "#",
-          field("name", /[a-zA-Z0-9_]+/),
-          field(
-            "value",
-            optional(choice($.absolute_path, $.relative_path, $.constant)),
-          ),
-          "\n",
+    identifier: ($) => /[a-zA-Z_][a-zA-Z0-9_]*/,
+    generic_identifier: ($) =>
+      seq($.identifier, "<", $.full_type_identifier, ">"),
+    full_type_identifier: ($) =>
+      seq(
+        field("module", repeat(seq(/[a-zA-Z_][a-zA-Z0-9_]*/, "::"))),
+        choice(
+          field("type", $.identifier),
+          field("generic_type", $.generic_identifier),
         ),
+      ),
+
+    string: ($) => seq('"', /[^"]*/, '"'),
+    array: ($) =>
+      seq(
+        "[",
+        optional(
+          seq(
+            $.full_type_identifier,
+            repeat(seq(",", $.full_type_identifier)),
+            optional(","),
+          ),
+        ),
+        "]",
       ),
 
     absolute_path: ($) => seq("<", /[^>]+/, ">"),
     relative_path: ($) => seq('"', /[^"]+/, '"'),
-    constant: ($) => /[a-zA-Z0-9_]+/,
-
-    identifier: ($) => /[a-zA-Z_][a-zA-Z0-9_]*/,
-
-    bind_operator: ($) => seq("-", ">"),
-
-    full_type_identifier: ($) =>
+    use_directive: ($) =>
       seq(
-        field("module", repeat(seq(/[a-zA-Z_][a-zA-Z0-9_]*/, "::"))),
-        field("type", $.identifier),
+        "#",
+        "use",
+        choice(
+          field("absolute", $.absolute_path),
+          field("relative", $.relative_path),
+        ),
+        "\n",
       ),
+
+    library_directive: ($) => seq("#", "library", "\n"),
+
+    any_value: ($) => choice($.string, $.array),
+    annotation: ($) =>
+      seq(
+        "@",
+        field("name", $.identifier),
+        optional(field("value", $.any_value)),
+      ),
+
+    metadata: ($) =>
+      seq(
+        field("identifier", $.identifier),
+        optional(field("value", seq("(", $.any_value, ")"))),
+      ),
+    metadata_list: ($) =>
+      seq(
+        "#",
+        "[",
+        $.metadata,
+        repeat(seq(",", $.metadata)),
+        optional(","),
+        "]",
+      ),
+
+    group_keyword: ($) => "group",
+    group_count: ($) =>
+      seq(
+        "(",
+        choice(seq($.integer, "-", $.integer), $.integer, "*", "?", "+"),
+        ")",
+      ),
+    group_bind: ($) =>
+      seq(
+        repeat($.annotation),
+        "+",
+        $.full_type_identifier,
+        optional($.group_count),
+      ),
+    group_definition: ($) =>
+      seq(
+        repeat(choice($.metadata_list, $.annotation)),
+        $.group_keyword,
+        field("identifier", $.identifier),
+        choice(seq("{", repeat($.group_bind), "}"), ";"),
+      ),
+
+    field_definition: ($) =>
+      seq(
+        repeat(choice($.metadata_list, $.annotation)),
+        field("field_identifier", $.identifier),
+        ":",
+        field("field_type", $.full_type_identifier),
+      ),
+    fields_definition: ($) =>
+      seq(
+        $.field_definition,
+        repeat(seq(",", $.field_definition)),
+        optional(","),
+      ),
+
+    element_keyword: ($) => "element",
+    element_definition: ($) =>
+      seq(
+        repeat(choice($.metadata_list, $.annotation)),
+        $.element_keyword,
+        field("identifier", $.identifier),
+        "-",
+        ">",
+        field("bind", $.full_type_identifier),
+        choice(seq("{", $.fields_definition, "}"), ";"),
+      ),
+
+    enum_keyword: ($) => "enum",
+    enum_variant: ($) =>
+      seq(
+        repeat(choice($.metadata_list, $.annotation)),
+        field("identifier", $.identifier),
+        optional(seq("(", field("inner_type", $.full_type_identifier), ")")),
+      ),
+    enum_variants: ($) =>
+      seq($.enum_variant, repeat(seq(",", $.enum_variant)), optional(",")),
+    enum_definition: ($) =>
+      seq(
+        repeat(choice($.metadata_list, $.annotation)),
+        $.enum_keyword,
+        field("identifier", $.identifier),
+        choice(seq("{", $.enum_variants, "}"), ";"),
+      ),
+
+    struct_keyword: ($) => "struct",
+    struct_definition: ($) =>
+      seq(
+        repeat(choice($.metadata_list, $.annotation)),
+        $.struct_keyword,
+        field("identifier", $.identifier),
+        choice(seq("{", $.fields_definition, "}"), ";"),
+      ),
+    struct_impl_keyword: ($) => seq("$", "struct"),
+    struct_impl: ($) =>
+      seq(
+        $.struct_impl_keyword,
+        field("identifier", $.identifier),
+        $.impl_bind_operator,
+        field("bind", $.full_type_identifier),
+        "{",
+        field("fields", $.fields_impl),
+        "}",
+      ),
+
+    expression_keyword: ($) => "expression",
+    expression_definition: ($) =>
+      seq(
+        repeat(choice($.metadata_list, $.annotation)),
+        $.expression_keyword,
+        field("identifier", $.identifier),
+        choice(seq("{", $.fields_definition, "}"), ";"),
+      ),
+
+    impl_bind_operator: ($) => seq("-", ">"),
+    expression_impl_keyword: ($) => seq("$", "expr"),
+    expression_impl: ($) =>
+      seq(
+        $.expression_impl_keyword,
+        field("identifier", $.identifier),
+        $.impl_bind_operator,
+        field("bind", $.full_type_identifier),
+        "{",
+        optional(field("fields", $.fields_impl)),
+        "}",
+      ),
+
+    integer: ($) => /-?[0-9]+/,
+    float: ($) => /-?[0-9]+\.[0-9]*/,
+    boolean: ($) => choice("true", "false"),
+    enum_variant_value: ($) => /[a-zA-Z_][a-zA-Z0-9_]*/,
+    custom_enum_variant_value: ($) => /[^"${}\[\],\/=> \s\t\r\n]+/,
 
     list: ($) =>
       seq(
@@ -76,46 +228,24 @@ module.exports = grammar({
         $.float,
         $.integer,
         $.string,
-        $.enum,
-        $.custom_enum,
+        $.enum_variant_value,
+        $.custom_enum_variant_value,
         $.list,
-        $.impl_ref,
+        //$.impl_ref,
       ),
 
-    field: ($) =>
+    field_impl: ($) =>
       seq(
         field("field_identifier", $.identifier),
         ":",
         field("field_value", $.field_value),
       ),
-    fields: ($) => seq($.field, repeat(seq(",", $.field)), optional(",")),
+    fields_impl: ($) =>
+      seq($.field_impl, repeat(seq(",", $.field_impl)), optional(",")),
 
-    struct_impl_keyword: ($) => seq("$", "struct"),
-    struct_impl: ($) =>
-      seq(
-        $.struct_impl_keyword,
-        field("identifier", $.identifier),
-        $.bind_operator,
-        field("bind", $.full_type_identifier),
-        "{",
-        field("fields", $.fields),
-        "}",
-      ),
-
-    expr_impl_keyword: ($) => seq("$", "expr"),
-    expr_impl: ($) =>
-      seq(
-        $.expr_impl_keyword,
-        field("identifier", $.identifier),
-        $.bind_operator,
-        field("bind", $.full_type_identifier),
-        "{",
-        optional(field("fields", $.fields)),
-        "}",
-      ),
-
-    expr: ($) => seq("{", $.full_type_identifier, $.fields, "}"),
-    struct: ($) => seq("{{", $.fields, "}}"),
+    expression_inline_impl: ($) =>
+      seq("{", $.full_type_identifier, $.fields_impl, "}"),
+    struct_inline_impl: ($) => seq("{{", $.fields_impl, "}}"),
 
     attribute_value: ($) =>
       choice(
@@ -123,12 +253,12 @@ module.exports = grammar({
         $.float,
         $.integer,
         $.string,
-        $.enum,
-        $.custom_enum,
+        $.enum_variant_value,
+        $.custom_enum_variant_value,
         $.list,
         $.impl_ref,
-        $.expr,
-        $.struct,
+        $.expression_inline_impl,
+        $.struct_inline_impl,
       ),
     attribute: ($) =>
       seq(
